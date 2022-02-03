64c984ae427f3a27fbaa139b058b089986f43c39
julienrichardalbert <jrichardalbert@gmail.com>
2022-02-03 15:12:32 +0100

    merge mouse and rat scripts

--- smooth_oocyte_DNAme.sh
+++ smooth_oocyte_DNAme.sh
@@ -5,9 +5,19 @@
 # filters for  1kb windows covered  by at least 5 CpGs over a span of 300 bp
 # reports the average methylation level for each 50bp step over an average of 1kb 
 
+# rat
 ~/bin/bedmapCoverage.sh rat_MII_oocyte_PBAT_BrindAmour2018_rep1-4_x5.bed5 rn6_50w_1000s.bed count
 ~/bin/bedmapCoverage.sh rat_MII_oocyte_PBAT_BrindAmour2018_rep1-4_x5.bed5 rn6_50w_1000s.bed mean
 ~/bin/bedmapCoverage.sh rat_MII_oocyte_PBAT_BrindAmour2018_rep1-4_x5.bed5 rn6_50w_1000s.bed echo-map-range
 awk '{OFS="\t";FS="\t"} { if ($4>=5 && $6>=300 && $3-$2>950) print $1, $2+475, $3-475, $5}' rn6_50w_1000s.bed > tmp
-bedGraphToBigWig tmp ~/sra2bw/reference_genomes/rn6/rn6.sizes ${x//.bw/_50s_1000w.bw}
+bedGraphToBigWig tmp ~/sra2bw/reference_genomes/rn6/rn6.sizes rat_MII_oocyte_PBAT_BrindAmour2018_rep1-4_x5_50w_1000s.bw
 
+# mouse
+~/bin/bedmapCoverage.sh BB_GVO_Shirane2013_total_methylation_x5.bed5 mm10_50w_1000s.bed count
+~/bin/bedmapCoverage.sh BB_GVO_Shirane2013_total_methylation_x5.bed5 mm10_50w_1000s.bed mean
+~/bin/bedmapCoverage.sh BB_GVO_Shirane2013_total_methylation_x5.bed5 mm10_50w_1000s.bed echo-map-range
+awk '{OFS="\t";FS="\t"} { if ($4>=5 && $6>=300 && $3-$2>950) print $1, $2+475, $3-475, $5}' mm10_50w_1000s.bed > tmp
+bedGraphToBigWig tmp ~/sra2bw/reference_genomes/mm10/mm10.sizes BB_GVO_Shirane2013_total_methylation_x5_50w_1000s.bw
+
+# methylation bigwigs were first converted to bed5 using the groomToBed5.sh script included in this repository
+# bedmapCoverage.sh is anothercustom script included in this repository
