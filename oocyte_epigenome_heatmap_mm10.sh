259042fb6538a2d35a41ef9f113a5ebdc545a0e9
julienrichardalbert <jrichardalbert@gmail.com>
2022-02-03 15:21:24 +0100

    add docs

--- oocyte_epigenome_heatmap_mm10.sh
+++ oocyte_epigenome_heatmap_mm10.sh
@@ -1,3 +1,7 @@
+# these were the commands used to generate the rat and mouse oocyte epigenome heatmaps
+# requires deeptools (computeMatrix and plotHeatmap are deeptools functions)
+
+# mouse
 computeMatrix scale-regions \
   -R mm10_NCBI_oocyte_RPKM_atleast1 mm10_NCBI_oocyte_RPKM_under1 \
   -S \
@@ -13,7 +17,6 @@ computeMatrix scale-regions \
   --binSize 100 \
   --numberOfProcessors 8
 
-
 plotHeatmap  \
   -m mouse_GVO_RPKM.gz \
   --colorMap Purples \
@@ -26,3 +29,32 @@ plotHeatmap  \
   --zMax 0.1 0.1 0.1 100  \
   --interpolationMethod nearest \
   --outFileName mouse_GVO_RPKM.pdf
+
+
+# rat
+computeMatrix scale-regions \
+  -R rn6_NCBI_oocyte_RPKM_oocyte_RPKM_atleast_1 rn6_NCBI_oocyte_RPKM_oocyte_RPKM_under_1 \
+  -S ./bigWigs/rep_merge/rat_GVO_H3K4me3_CnR_rep1-2_rmDup_q10_b1_s0_CPM.bw \
+  ./bigWigs/rep_merge/rat_GVO_H3K27me3_CnR_rep1-2_rmDup_q10_b1_s0_CPM.bw \
+  ./bigWigs/rep_merge/rat_GVO_H3K36me3_CnR_rep1-2_rmDup_q10_b1_s0_CPM.bw \
+  ./bigWigs/rep_merge/rat_MII_oocyte_PBAT_BrindAmour2018_rep1-4_x5.bw \
+  -b 5000 \
+  -a 5000 \
+  --regionBodyLength 5000 \
+  -o rn6_by_oocyte_RPKM.gz \
+  --skipZeros \
+  --binSize 100 \
+  --numberOfProcessors 8
+
+plotHeatmap  \
+  -m rn6_by_oocyte_RPKM.gz \
+  --colorMap Purples \
+  --missingDataColor "#FFF6EB"  \
+  --heatmapHeight 15 \
+  --heatmapWidth 10  \
+  --yMin 0 0 0 0 \
+  --zMin 0 0 0 0  \
+  --yMax 0.2 0.1 0.2 100  \
+  --zMax 0.2 0.1 0.2 100  \
+  --interpolationMethod nearest \
+  --outFileName rn6_by_oocyte_RPKM.pdf
